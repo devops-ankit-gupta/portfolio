56c01542e5ebd4e9b3949c721c29a769ce89fe3f
devops-ankit-gupta <devopsankitgupta@gmail.com>
2025-04-25 18:19:57 +0530

    fix: scroll issue

--- src/component/scrollup/ScrollUp.jsx
+++ src/component/scrollup/ScrollUp.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./scrollup.css"
 
 const ScrollUp = () => {
-    window.addEventListener("scroll", function() {
-        const scrollUp = document.querySelector(".scrollup");
-        if (this.scrollY >= 560) scrollUp.classList.add ("show-scroll");
-        else scrollUp.classList.remove("show-scroll");
-    })
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollUp = document.querySelector(".scrollup");
+      if (window.scrollY >= 560) {
+        scrollUp.classList.add("show-scroll");
+      } else {
+        scrollUp.classList.remove("show-scroll");
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const handleClick = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
 
   return (
-    <a href="#" className="scrollup">
-        <i className="uil uil-arrow-up scrollup_icon"></i>
-    </a>
+    <button onClick={handleClick} className="scrollup" aria-label="Scroll to top">
+      <i className="uil uil-arrow-up scrollup_icon"></i>
+    </button>
   )
 }
 
